284b17d62639b96b2d03f29e21da4c04455dab20
Anshu Gupta <anshuraj77346@gmail.com>
2026-03-10 23:10:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gita-wisdom-v5';
+const CACHE = 'gita-wisdom-v6';
 const CORE = ['./', './index.html', './manifest.json', './data/gita.json', './icons/icon-192x192.svg', './icons/icon-512x512.svg'];
 
 self.addEventListener('install', e => {
